67a3de75a0e6880242b1bd7f12e494d656834cde
sagewall <sagemaps@gmail.com>
2018-08-09 14:14:18 -0600

    Placing the template

--- src/app/home/welcome.component.js
+++ src/app/home/welcome.component.js
@@ -14,7 +14,6 @@ var WelcomeComponent = (function () {
 }());
 WelcomeComponent = __decorate([
     core_1.Component({
-        selector: 'app-home',
         templateUrl: './app/home/welcome.component.html'
     })
 ], WelcomeComponent);
