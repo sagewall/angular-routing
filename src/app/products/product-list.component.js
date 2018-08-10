7347c66e25cc0af90fd9161418a0a7ffe8dff216
Sage Wall <sagewall@users.noreply.github.com>
2018-08-10 15:55:49 -0600

    Reading query parameters

--- src/app/products/product-list.component.js
+++ src/app/products/product-list.component.js
@@ -9,10 +9,12 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var product_service_1 = require("./product.service");
 var ProductListComponent = (function () {
-    function ProductListComponent(productService) {
+    function ProductListComponent(productService, route) {
         this.productService = productService;
+        this.route = route;
         this.pageTitle = 'Product List';
         this.imageWidth = 50;
         this.imageMargin = 2;
@@ -23,6 +25,8 @@ var ProductListComponent = (function () {
     };
     ProductListComponent.prototype.ngOnInit = function () {
         var _this = this;
+        this.listFilter = this.route.snapshot.queryParams['filterBy'] || '';
+        this.showImage = this.route.snapshot.queryParams['showImage'] === 'true';
         this.productService.getProducts()
             .subscribe(function (products) { return _this.products = products; }, function (error) { return _this.errorMessage = error; });
     };
@@ -33,7 +37,8 @@ ProductListComponent = __decorate([
         templateUrl: './app/products/product-list.component.html',
         styleUrls: ['./app/products/product-list.component.css']
     }),
-    __metadata("design:paramtypes", [product_service_1.ProductService])
+    __metadata("design:paramtypes", [product_service_1.ProductService,
+        router_1.ActivatedRoute])
 ], ProductListComponent);
 exports.ProductListComponent = ProductListComponent;
 //# sourceMappingURL=product-list.component.js.map
